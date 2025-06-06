725a154fc9ddcd5b1fe292bae902db8595f5c472
Vitalij1982 <cdmason2002@yahoo.com>
2025-06-06 21:04:44 +0300

    33.py

--- 33.py
+++ 33.py
@@ -3,9 +3,9 @@ import tkinter as tk
 root = tk.Tk()
 root.title("Обновление")
 root.geometry("800x600")
-root.configure(bg="red")  # Красный фон
+root.configure(bg="red")
 
-label = tk.Label(root, text="Программа обновлена", font=("Arial", 28), fg="white", bg="red")
+label = tk.Label(root, text="Программа обновлена222", font=("Arial", 28), fg="white", bg="red")
 label.pack(expand=True)
 
 root.mainloop()
