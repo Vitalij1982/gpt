d3ced7948bc8a9a51c8aed67eeddfa586bad7464
Vitalij1982 <cdmason2002@yahoo.com>
2025-06-06 20:57:31 +0300

    Update 33.py

--- 33.py
+++ 33.py
@@ -3,9 +3,9 @@ import tkinter as tk
 root = tk.Tk()
 root.title("Обновление")
 root.geometry("800x600")
-root.configure(bg="blue")  # Устанавливаем синий фон
+root.configure(bg="red")  # Красный фон
 
-label = tk.Label(root, text="Программа обновлена", font=("Arial", 28), fg="white", bg="blue")
+label = tk.Label(root, text="Программа обновлена", font=("Arial", 28), fg="white", bg="red")
 label.pack(expand=True)
 
 root.mainloop()
