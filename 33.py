2a5f46c96649b9789124916a8936bddd0491d44f
Vitalij1982 <cdmason2002@yahoo.com>
2025-06-06 20:18:23 +0300

    Update 33.py

--- 33.py
+++ 33.py
@@ -1,6 +1,10 @@
 import tkinter as tk
 
 root = tk.Tk()
-root.configure(bg="blue")
-root.geometry("800x600")
+root.title("Программа 33")
+root.geometry("400x300")
+
+label = tk.Label(root, text="24", font=("Arial", 72), fg="black")
+label.pack(expand=True)
+
 root.mainloop()
