4b38340a95f7680163ab33340579c58a12a9d5d2
Vitalij1982 <cdmason2002@yahoo.com>
2025-06-06 21:19:09 +0300

    Update 33.py

--- 33.py
+++ 33.py
@@ -5,7 +5,7 @@ root.title("Обновление")
 root.geometry("800x600")
 root.configure(bg="red")
 
-label = tk.Label(root, text="Программа обновлена222", font=("Arial", 28), fg="white", bg="red")
+label = tk.Label(root, text="Программа обновлена222333", font=("Arial", 28), fg="white", bg="red")
 label.pack(expand=True)
 
 root.mainloop()
